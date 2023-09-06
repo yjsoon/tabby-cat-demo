1bcf96fc6aead80c4de458dd4b9911c91e338aab
YJ Soon <yjsoon@tinkertanker.com>
2023-09-06 10:13:01 +0800

    Initial Commit

---
+++ Tabby Cat Demo 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tabby Cat Demo 2
+//
+//  Created by YJ Soon on 6/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tabby Cat Demo 2/Tabby_Cat_Demo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_Cat_Demo_2App.swift
+//  Tabby Cat Demo 2
+//
+//  Created by YJ Soon on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_Cat_Demo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
