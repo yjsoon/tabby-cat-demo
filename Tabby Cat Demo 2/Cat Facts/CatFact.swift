99b1990cbc68629583938a847a39bf36de9b826f
YJ Soon <yjsoon@tinkertanker.com>
2023-09-06 11:06:14 +0800

    Set up CatFactsManager and CatFact

---
+++ Tabby Cat Demo 2/Cat Facts/CatFact.swift
@@ -0,0 +1,17 @@
+//
+//  CatFact.swift
+//  Tabby Cat Demo 2
+//
+//  Created by YJ Soon on 6/9/23.
+//
+
+import Foundation
+
+struct CatFact: Codable {
+    
+    var fact: String
+    var length: Int
+    
+    // var numberOfCats: Int // "number_of_cats": 5
+    
+}
--- Tabby Cat Demo 2/Cat Facts/CatFactsManager.swift
+++ Tabby Cat Demo 2/Cat Facts/CatFactsManager.swift
@@ -10,15 +10,22 @@ import Foundation
 class CatFactsManager: ObservableObject {
     
     let apiURL = URL(string: "https://catfact.ninja/fact")!
+    @Published var catFact: CatFact?
     
     func getCatFact() {
+        
+        catFact = nil
         
         Task {
             let (data, response) = try await URLSession.shared.data(from: apiURL)
             print("🐱 GOT CAT FACT!!! 🐱")
             print(data)
             print(String(data: data, encoding: .utf8)!)
-//            print(response)
+
+            // update the UI - go back to the main thread
+            try await MainActor.run {
+                catFact = try JSONDecoder().decode(CatFact.self, from: data)
+            }
         }
         
     }
